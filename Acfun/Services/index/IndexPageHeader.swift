af0905bf2e99b49f2bc860c22e02f1c5ac328d27
= <=>
2017-01-24 22:31:18 +0800

    TopHeader

---
+++ Acfun/Services/index/IndexPageHeader.swift
@@ -0,0 +1,22 @@
+//
+//  IndexPageHeader.swift
+//  Acfun
+//
+//  Created by 魏星 on 2017/1/24.
+//  Copyright © 2017年 wx. All rights reserved.
+//
+
+import UIKit
+
+class IndexPageHeader: UICollectionReusableView {
+
+    @IBOutlet weak var pageScrollView: UIScrollView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
