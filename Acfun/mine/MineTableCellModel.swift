c9370d18dc116fe97425db9565c6671a5c6c2b8a
= <=>
2017-02-13 23:59:53 +0800

    Mine

---
+++ Acfun/mine/MineTableCellModel.swift
@@ -0,0 +1,15 @@
+//
+//  MineTableCellModel.swift
+//  Acfun
+//
+//  Created by 魏星 on 2017/2/13.
+//  Copyright © 2017年 wx. All rights reserved.
+//
+
+import Foundation
+
+struct MineTableCellModel {
+    var icon: UIImage!
+    var title: String!
+    var rightDesc: String?
+}
--- Acfun/mine/MineViewController.swift
+++ Acfun/mine/MineViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 //我的
 class MineViewController: UIViewController {
 
+    
+    @IBOutlet weak var mineTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupNavBar()
+        setUpTableview()
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +25,75 @@ class MineViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
     }
-    */
+    
+    
+    private func setupNavBar(){
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    private func setUpTableview(){
+        self.mineTableView.delegate = self
+        self.mineTableView.dataSource = self
+        self.mineTableView.backgroundView = nil
+        self.mineTableView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        self.mineTableView.isOpaque = false
+        
+    }
+    
+}
 
+extension MineViewController: UITableViewDelegate,UITableViewDataSource{
+    
+    
+    var cellDatas:[[MineTableCellModel]] {
+        return [
+            [MineTableCellModel(icon: #imageLiteral(resourceName: "ic_collection"), title: "我的收藏", rightDesc:"登陆解开封印"),
+            MineTableCellModel(icon: #imageLiteral(resourceName: "ic_history"), title: "历史记录", rightDesc:nil),
+            MineTableCellModel(icon: #imageLiteral(resourceName: "ic_download"), title: "离线缓存", rightDesc:nil)]
+            ,[MineTableCellModel(icon: #imageLiteral(resourceName: "ic_setting"), title: "设置", rightDesc:nil),
+              MineTableCellModel(icon: #imageLiteral(resourceName: "ic_feedback"), title: "意见反馈", rightDesc:nil)]
+            ,[MineTableCellModel(icon: #imageLiteral(resourceName: "my_game_center_color_gray"), title: "游戏中心", rightDesc:nil)]
+        ]
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return cellDatas.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellDatas[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = cellDatas[indexPath.section][indexPath.row]
+        
+        var cell: UITableViewCell
+
+        if let desc = model.rightDesc{
+            if let mineCell = tableView.dequeueReusableCell(withIdentifier: "MineCell"){
+                cell = mineCell
+            }else{
+                cell = UITableViewCell(style: .value1, reuseIdentifier: "MineCell")
+            }
+            
+            cell.detailTextLabel?.text = desc
+            cell.accessoryType = .none
+
+        }else{
+            if let mineCell = tableView.dequeueReusableCell(withIdentifier: "MineCell"){
+                cell = mineCell
+            }else{
+                cell = UITableViewCell(style: .default, reuseIdentifier: "MineCell")
+            }
+            cell.accessoryType = .disclosureIndicator
+
+        }
+        cell.textLabel?.text = model.title
+        cell.imageView?.image = model.icon
+        return cell
+    }
 }
